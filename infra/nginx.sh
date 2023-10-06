915f5361de4b8de36a1b690588f8eecb17de8a72
Wombatoff <mwombatoff@gmail.com>
2023-10-06 21:02:52 +0300

    setup NGINX

--- infra/nginx.sh
+++ infra/nginx.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
+set -e
 
-apt update
+apt-get update
 
-apt install -y nginx
+apt-get install -y nginx
+
+[ ! -d "/var/html" ] && mkdir -p /var/html
+
+cp -r /var/lib/docker/volumes/infra_frontend_static_data/_data /var/html/infra_frontend_static_data
+cp -r /var/lib/docker/volumes/infra_backend_static_data/_data /var/html/infra_backend_static_data
+cp -r /var/lib/docker/volumes/infra_media_data/_data /var/html/infra_media_data
+
+chown -R www-data:www-data /var/html/infra_frontend_static_data
+chown -R www-data:www-data /var/html/infra_backend_static_data
+chown -R www-data:www-data /var/html/infra_media_data
+
+chmod -R 755 /var/html/infra_frontend_static_data
+chmod -R 755 /var/html/infra_backend_static_data
+chmod -R 755 /var/html/infra_media_data
 
 if [ -f "./nginx.conf" ]; then
     cp ./nginx.conf /etc/nginx/sites-available/dvmn_7
 
-    if [ $? -eq 0 ]; then
-        [ ! -d "/etc/nginx/sites-enabled/" ] && mkdir /etc/nginx/sites-enabled/
+    [ ! -d "/etc/nginx/sites-enabled/" ] && mkdir /etc/nginx/sites-enabled/
 
-        [ -L "/etc/nginx/sites-enabled/dvmn_7" ] && rm /etc/nginx/sites-enabled/dvmn_7
+    [ -L "/etc/nginx/sites-enabled/dvmn_7" ] && rm /etc/nginx/sites-enabled/dvmn_7
 
-        ln -s /etc/nginx/sites-available/dvmn_7 /etc/nginx/sites-enabled/
+    ln -s /etc/nginx/sites-available/dvmn_7 /etc/nginx/sites-enabled/
 
-        [ -f "/etc/nginx/sites-enabled/default" ] && rm /etc/nginx/sites-enabled/default
+    [ -f "/etc/nginx/sites-enabled/default" ] && rm /etc/nginx/sites-enabled/default
 
-        nginx -t
+    nginx -t
 
-        if [ $? -eq 0 ]; then
-            systemctl restart nginx
-            systemctl enable nginx
-            echo "NGINX is installed with the custom configuration."
-            systemctl status nginx
-        else
-            echo "There is an error in the NGINX configuration."
-        fi
-    else
-        echo "Failed to copy the configuration file."
-    fi
+    systemctl restart nginx
+    systemctl enable nginx
+    echo "NGINX is installed with the custom configuration."
+    systemctl status nginx
 else
     echo "Configuration file not found in the current directory."
 fi
